b34aa876e70f0f7d850a086d63e5ebd50db28c41
Diego Andrade Canosa <diego.andrade@udc.es>
2022-12-13 05:21:27 +0100

    scriptsDisks.sh updated

--- curso_1nodecpu/scriptsDisks.sh
+++ curso_1nodecpu/scriptsDisks.sh
@@ -7,6 +7,8 @@ echo "  STORE"
 dd if=/dev/zero of=$STORE/test1.img bs=1G count=10 oflag=dsync
 echo "  LUSTRE"
 dd if=/dev/zero of=$LUSTRE/test1.img bs=1G count=10 oflag=dsync
+echo "  LUSTRE_SCRATCH"
+dd if=/dev/zero of=$LUSTRE_SCRATCH/test1.img bs=1G count=10 oflag=dsync
 echo "Reading..."
 #echo "  HOME"
 #dd of=/dev/zero if=$HOME/test1.img bs=1G count=10 oflag=dsync
@@ -14,7 +16,10 @@ echo "  STORE"
 dd of=/dev/zero if=$STORE/test1.img bs=1G count=10 oflag=dsync
 echo "  LUSTRE"
 dd of=/dev/zero if=$LUSTRE/test1.img bs=1G count=10 oflag=dsync
+echo "  LUSTRE SCRATCH"
+dd of=/dev/zero if=$LUSTRE_SCRATCH/test1.img bs=1G count=10 oflag=dsync
 echo "Cleaning..."
 #rm $HOME/test1.img
 rm $STORE/test1.img
 rm $LUSTRE/test1.img
+rm $LUSTRE_SCRATCH/test1.img
