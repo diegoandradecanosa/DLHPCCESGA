27f79b933cf2b4b4b9100f6c852b9fdf977e5767
Diego Andrade Canosa <diego.andrade@udc.es>
2022-12-14 08:11:09 +0100

    Small fix

--- curso_1nodecpu/condaTorch.sh
+++ curso_1nodecpu/condaTorch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load miniconda3
-conda env remove -n mytorch python=3.9
-conda create -y -n mytorch 
+conda env remove -n mytorch 
+conda create -y -n mytorch python=3.9
 conda activate mytorch
 conda install -y pytorch torchvision torchaudio -c pytorch
